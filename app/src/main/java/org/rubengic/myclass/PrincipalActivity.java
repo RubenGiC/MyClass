c8ff243bb50ef5561967c5ea3fb8e45415532088
RubenGiC <rubengirela@gmail.com>
2021-10-19 11:33:06 +0200

    Añadido parte del JSON en android

--- app/src/main/java/org/rubengic/myclass/PrincipalActivity.java
+++ app/src/main/java/org/rubengic/myclass/PrincipalActivity.java
@@ -2,23 +2,41 @@ package org.rubengic.myclass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     TextView titulo;
+    int id_alumno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-        int id_alumno = getIntent().getExtras().getInt("id");
+        id_alumno = getIntent().getExtras().getInt("id");
 
         titulo = (TextView) findViewById(R.id.tv_titulo);
 
@@ -50,5 +68,63 @@ public class PrincipalActivity extends AppCompatActivity {
         }
 
         titulo.setText(titulo_s);
+    }
+
+    //para validar el usuario y contraseña
+    private void obtenerListaJSON(String URL){
+        //genero el request de tipo POST
+        JsonArrayRequest sr = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            /**
+             * metodo para recibir la respuesta del servidor PHP
+             * @param response (recibe el mensaje del servidor)
+             */
+            @Override
+            public void onResponse(JSONArray response) {
+
+                JSONObject json_object = null;
+
+                //si no esta vacia, tiene asignaturas
+                if(response.length()>0){
+
+                    //recorro los valores del json
+                    for(int i = 0; i<response.length(); ++i){
+                        try {
+                            json_object = response.getJSONObject(i);
+                        }catch (JSONException e){
+                            Toast.makeText(PrincipalActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }else{
+                    //si no devuelve nada el servidor es que no tiene asignaturas ese dia
+                    Toast.makeText(PrincipalActivity.this, "Hoy no tiene clase", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                //en caso de error en la respuesta muestro un toast del error
+                Toast.makeText(PrincipalActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            /**
+             * Aqui generamos el JSON para enviarlo al server PHP
+             * @return devuelve el Map
+             * @throws AuthFailureError
+             */
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //creo el mapa de tipo string
+                Map<String,String> parametros = new HashMap<String,String>();
+                //e introduzco los parametros usuario y contraseña
+                parametros.put("id_usuario",String.valueOf(id_alumno));
+                //parametros.put("password",ed_password.getText().toString());
+                return parametros;
+            }
+        };
+        //creo la instancia del request para procesar las peticiones a traves de aqui
+        RequestQueue rq = Volley.newRequestQueue(this);
+        rq.add(sr);
     }
 }
